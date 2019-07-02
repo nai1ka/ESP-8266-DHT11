a46f7a0671b4af41fc1764d8c4d1e9b15fa81a1c
Nai1ka <40440192+Nai1ka@users.noreply.github.com>
2019-07-02 13:32:09 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import datetime
+import os
 
 
 app = Flask(__name__)
@@ -30,4 +31,5 @@ def temperature():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port)
